5da73f67c4684558df50f1c453123231eff3a52b
cytoplasma <jpiromg@gmail.com>
2023-01-26 14:23:50 -0800

    .env, connecting to database via sequelize

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
-const mysql = require("mysql2");
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "9038291Jr!~~",
-    database: "employees"
-});
+const { Sequelize } = require("sequelize");
+require("dotenv").config({ path: "../.env" });
 
-module.exports = connection;
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: "localhost",
+        dialect: "mysql",
+        port: 3306,
+    }
+);
+
+module.exports = sequelize;
